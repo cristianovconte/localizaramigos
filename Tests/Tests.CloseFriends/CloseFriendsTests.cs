8574def54e4ea417ce74f436639e003b26c592ac
cconte <cconte@WST-118>
2018-05-21 19:43:58 -0300

    Testes unitários no serviço de Autenticação

--- Tests/Tests.CloseFriends/CloseFriendsTests.cs
+++ Tests/Tests.CloseFriends/CloseFriendsTests.cs
@@ -1,22 +1,66 @@
+using CrossCutting.Authentication;
+using Newtonsoft.Json;
 using System;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using Xunit;
 
 namespace Tests.CloseFriends
 {
     public class CloseFriendsTests
     {
-        [Fact]
-        public void Authentication_Failed()
-        {
+        const string urlAPI = "http://localhost:22761/api/";
 
+        [Fact]
+        public void APIAuthentication_Fail()
+        {
+            bool authentication = false;
+            string userId = "user1";
+            string accessKey = "53nh@123898*";
+
+            authentication = Authentication(authentication, userId, accessKey);
+
+            Assert.False(authentication);
         }
 
         [Fact]
-        public void Authentication_Sucess()
-        { }
+        public void APIAuthentication_Sucess()
+        {
+            bool authentication = false;
+            string userId = "user1";
+            string accessKey = "53nh@123*";
 
-        [Fact]
-        public void CloseFriends_Top3()
-        { }
+            authentication = Authentication(authentication, userId, accessKey);
+
+            Assert.True(authentication);
+        }
+
+        private static bool Authentication(bool authentication, string userId, string accessKey)
+        {
+            using (var client = new HttpClient())
+            {
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(
+                    new MediaTypeWithQualityHeaderValue("application/json"));
+
+                HttpResponseMessage respToken = client.PostAsync(
+                    urlAPI + "authentication", new StringContent(
+                        JsonConvert.SerializeObject(new
+                        {
+                            UserID = userId,
+                            AccessKey = accessKey
+                        }), System.Text.Encoding.UTF8, "application/json")).Result;
+
+                string content = respToken.Content.ReadAsStringAsync().Result;
+
+                if (respToken.StatusCode == System.Net.HttpStatusCode.OK)
+                {
+                    Token token = JsonConvert.DeserializeObject<Token>(content);
+                    authentication = token.Authenticated;
+                }
+            }
+
+            return authentication;
+        }
     }
 }
